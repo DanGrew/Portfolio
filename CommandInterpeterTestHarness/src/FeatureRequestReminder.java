d6b6d815d498eccf06b75d48324904b53451ba05
DanGrew <danielanthonygrew@gmail.com>
2015-05-22 18:43:27 +0100

    FEATURES: Added failing tests to track features required.

---
+++ CommandInterpeterTestHarness/src/FeatureRequestReminder.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FeatureRequestReminder {
+
+   @Test public void keyDefinition() {
+      //More than just a String.
+      fail( "Not yet implemented" );
+   }
+   
+   @Test public void singletonResolutionInFunctions() {
+      //Handle if they exist - todos.
+      fail( "Not yet implemented" );
+   }
+   
+   @Test public void commandsThatCanBuildCommands() {
+      fail( "Not yet implemented" );
+   }
+   
+   @Test public void basicCommandBuilding() {
+      fail( "Not yet implemented" );
+   }
+   
+}
---
+++ ObjectBuilderTestHarness/src/FeatureRequestReminder.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FeatureRequestReminder {
+
+   @Test public void searchFunctionForSingletons() {
+      fail( "Not yet implemented" );
+   }
+   
+   @Test public void searchForValueInPropertyType() {
+      fail( "Not yet implemented" );
+      
+   }
+   @Test public void arithmeticOnNumberProperties() {
+      fail( "Not yet implemented" );
+   }
+   
+   @Test public void builtObjectViewer() {
+      fail( "Not yet implemented" );
+   }
+   
+}
