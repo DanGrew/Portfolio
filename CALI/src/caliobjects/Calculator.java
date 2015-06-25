a43dc24177b7e83b430d748fedf84bbe1de4031e
DanGrew <danielanthonygrew@gmail.com>
2015-06-25 08:27:00 +0100

    CALCULATOR FUNCTIONS: Added subtract, clear and multiply functions to calculator.

--- CALI/src/caliobjects/Calculator.java
+++ CALI/src/caliobjects/Calculator.java
@@ -8,7 +8,6 @@
 package caliobjects;
 
 import command.Command;
-
 import model.singleton.SingletonImpl;
 import annotation.Cali;
 
@@ -18,6 +17,7 @@ import annotation.Cali;
 @Cali
 public class Calculator extends SingletonImpl< SerializableCalculator > {
 
+   private static final String NOT_EVALUATED = "NotEvaluated";
    private double result;
    
    /**
@@ -48,39 +48,119 @@ public class Calculator extends SingletonImpl< SerializableCalculator > {
    @Override protected void readSingleton( SerializableCalculator serialized ) {}
 
    /**
+    * Getter for the current result in the {@link Calculator}.
+    * @return the current result, that is a double, as a {@link String}.
+    */
+   @Cali public String getResult() {
+      return "" + result;
+   }// End Method
+   
+   /**
     * Method to add two values together, input as strings.
     * @param stringA the {@link String} representing the first value.
     * @param stringB the {@link String} representing the second value.
+    * @return the result of the addition.
     */
-   @Cali public void add( String stringA, String stringB ) {
+   @Cali public String add( String stringA, String stringB ) {
       try {
          Double valueA = Double.valueOf( stringA );
          Double valueB = Double.valueOf( stringB );
-         setResult( valueA + valueB );
+         return "" + ( valueA + valueB );
       } catch ( NumberFormatException exception ) {
-         return;
+         return NOT_EVALUATED;
       }
    }// End Method
 
    /**
     * Method to add the given value to the current result.
     * @param string the {@link String} representing the value to add.
+    * @return the current {@link Calculator#getResult()}.
     */
-   @Cali public void add( String string ) {
+   @Cali public String add( String string ) {
       try {
          Double value = Double.valueOf( string );
          setResult( getResultValue() + value );
+      } catch ( NumberFormatException exception ) {}
+      return getResult();
+   }// End Method
+
+   /**
+    * Method to subtract the second parameter from the first.
+    * @param stringA the value to subtract from.
+    * @param stringB the value to subtract.
+    * @return the result of the subtraction.
+    */
+   @Cali public String subtract( String stringA, String stringB ) {
+      try {
+         Double valueA = Double.valueOf( stringA );
+         Double valueB = Double.valueOf( stringB );
+         return "" + ( valueA - valueB );
       } catch ( NumberFormatException exception ) {
-         return;
+         return NOT_EVALUATED;
       }
    }// End Method
-    
+
    /**
-    * Getter for the current result in the {@link Calculator}.
-    * @return the current result, that is a double, as a {@link String}.
+    * Method to subtract the parameter from the current {@link Calculator#getResult()}.
+    * @param string the value to subtract.
     */
-   @Cali public String getResult() {
-      return "" + result;
+   @Cali public String subtract( String string ) {
+      try {
+         Double value = Double.valueOf( string );
+         setResult( getResultValue() - value );
+      } catch ( NumberFormatException exception ) {}
+      return getResult();
+   }// End Method
+
+   /**
+    * Method to clear the {@link Calculator}, to zero.
+    * @return the current {@link Calculator#getResult()}.
+    */
+   @Cali public String clear() {
+      setResult( 0.0 );
+      return getResult();
+   }// End Method
+
+   /**
+    * Method to clear the {@link Calculator} to the given value.
+    * @param string the value to clear the calculator to.
+    * @return the {@link Calculator#getResult()}.
+    */
+   @Cali public String clear( String string ) {
+      try {
+         Double value = Double.valueOf( string );
+         setResult( value );
+      } catch ( NumberFormatException exception ) {}
+      return getResult();
+   }// End Method
+
+   /**
+    * Method to multiple the given parameters.
+    * @param stringA the first parameter to multiply.
+    * @param stringB the second parameter to multiply.
+    * @return the result of the multiplication.
+    */
+   @Cali public String multiply( String stringA, String stringB ) {
+      try {
+         Double valueA = Double.valueOf( stringA );
+         Double valueB = Double.valueOf( stringB );
+         return "" + ( valueA * valueB );
+      } catch ( NumberFormatException exception ) {
+         return NOT_EVALUATED;
+      }
+   }// End Method
+
+   /**
+    * Method to multiple the {@link Calculator#getResult()} by the given value.
+    * @param string the value to multiply by.
+    * @return the {@link Calculator#getResult()}.
+    */
+   @Cali public String multiply( String string ) {
+      try {
+         Double value = Double.valueOf( string );
+         setResult( getResultValue() * value );
+      } catch ( NumberFormatException exception ) {}
+      return getResult();
    }// End Method
 
 }// End Class
--- CaliTestHarness/src/caliobjects/CalculatorTest.java
+++ CaliTestHarness/src/caliobjects/CalculatorTest.java
@@ -29,8 +29,8 @@ public class CalculatorTest {
     */
    @Test public void shouldAdd(){
       Calculator calculator = new Calculator( "anything" );
-      calculator.add( "34", "10" );
-      Assert.assertEquals( "44.0", calculator.getResult() );
+      Assert.assertEquals( "44.0", calculator.add( "34", "10" ) );
+      Assert.assertEquals( "0.0", calculator.getResult() );
    }// End Method
    
    /**
@@ -39,14 +39,92 @@ public class CalculatorTest {
    @Test public void shouldAddCumulatively(){
       Calculator calculator = new Calculator( "anything" );
       
+      Assert.assertEquals( "34.0", calculator.add( "34" ) );
+      Assert.assertEquals( "34.0", calculator.getResult() );
+      
+      Assert.assertEquals( "90.0", calculator.add( "56" ) );
+      Assert.assertEquals( "90.0", calculator.getResult() );
+      
+      Assert.assertEquals( "824.7", calculator.add( "734.7" ) );
+      Assert.assertEquals( "824.7", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#subtract(String, String)} acceptance test.
+    */
+   @Test public void shouldSubtract(){
+      Calculator calculator = new Calculator( "anything" );
+      Assert.assertEquals( "24.0", calculator.subtract( "34", "10" ) );
+      Assert.assertEquals( "0.0", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#subtract(String)} acceptance test.
+    */
+   @Test public void shouldSubtractFromResult(){
+      Calculator calculator = new Calculator( "anything" );
+      
+      Assert.assertEquals( "-34.0", calculator.subtract( "34" ) );
+      Assert.assertEquals( "-34.0", calculator.getResult() );
+      
+      Assert.assertEquals( "-90.0", calculator.subtract( "56" ) );
+      Assert.assertEquals( "-90.0", calculator.getResult() );
+      
+      Assert.assertEquals( "-824.7", calculator.subtract( "734.7" ) );
+      Assert.assertEquals( "-824.7", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#clear()} acceptance test.
+    */
+   @Test public void shouldClearToZero(){
+      Calculator calculator = new Calculator( "anything" );
+      
       calculator.add( "34" );
       Assert.assertEquals( "34.0", calculator.getResult() );
       
-      calculator.add( "56" );
-      Assert.assertEquals( "90.0", calculator.getResult() );
+      Assert.assertEquals( "0.0", calculator.clear() );
+      Assert.assertEquals( "0.0", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#clear(String)} acceptance test.
+    */
+   @Test public void shouldClearToValue(){
+      Calculator calculator = new Calculator( "anything" );
       
-      calculator.add( "734.7" );
-      Assert.assertEquals( "824.7", calculator.getResult() );
+      calculator.add( "34" );
+      Assert.assertEquals( "34.0", calculator.getResult() );
+      
+      Assert.assertEquals( "20.0", calculator.clear( "20.0" ) );
+      Assert.assertEquals( "20.0", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#multiply(String, String)} acceptance test.
+    */
+   @Test public void shouldMultiply(){
+      Calculator calculator = new Calculator( "anything" );
+      Assert.assertEquals( "340.0", calculator.multiply( "34", "10" ) );
+      Assert.assertEquals( "0.0", calculator.getResult() );
+   }// End Method
+   
+   /**
+    * {@link Calculator#multiply(String)} acceptance test.
+    */
+   @Test public void shouldMultiplyResult(){
+      Calculator calculator = new Calculator( "anything" );
+      
+      calculator.clear( "4.0" );
+      
+      Assert.assertEquals( "8.0", calculator.multiply( "2" ) );
+      Assert.assertEquals( "8.0", calculator.getResult() );
+      
+      Assert.assertEquals( "16.0", calculator.multiply( "2" ) );
+      Assert.assertEquals( "16.0", calculator.getResult() );
+      
+      Assert.assertEquals( "64.0", calculator.multiply( "4" ) );
+      Assert.assertEquals( "64.0", calculator.getResult() );
    }// End Method
 
 }// End Class
