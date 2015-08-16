418a9cbc32258444286e4b9b5f7284e941146a67
DanGrew <danielanthonygrew@gmail.com>
2015-08-16 09:27:29 +0100

    ISSUE#11 GRAPH WIZARD DROP DOWN: Implement combo box for graph name selection for editing existing graphs.

--- ObjectBuilderEnvironment/src/wizard/graph/GraphIntroductionPage.java
+++ ObjectBuilderEnvironment/src/wizard/graph/GraphIntroductionPage.java
@@ -7,17 +7,17 @@
  */
 package wizard.graph;
 
+import java.util.Arrays;
+import java.util.List;
+
+import architecture.request.RequestSystem;
 import graphics.JavaFx;
 import graphics.wizard.Wizard;
 import graphics.wizard.WizardPage;
 import graphs.graph.Graph;
-
-import java.util.Arrays;
-
 import javafx.scene.Node;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
-import architecture.request.RequestSystem;
 
 /**
  * The {@link GraphIntroductionPage} is responsible for introducing the user to the
@@ -26,7 +26,7 @@ import architecture.request.RequestSystem;
 public class GraphIntroductionPage extends VBox implements WizardPage< Graph > {
 
    private static final String ERROR_HEADER = "Graph name needs some work!";
-   private TextField nameField;
+   private ComboBox< String > graphBox;
    
    /**
     * Constructs a new {@link GraphIntroductionPage}.
@@ -47,12 +47,24 @@ public class GraphIntroductionPage extends VBox implements WizardPage< Graph > {
          )
       ) );
       
-      nameField = new TextField();
-      getChildren().add( nameField );
+      graphBox = new ComboBox<>();
+      graphBox.setPrefWidth( GraphWizardConfiguration.wizardWidth() );
+      graphBox.setEditable( true );
+      refreshGraphs();
+      getChildren().add( graphBox );
       
       setPrefWidth( GraphWizardConfiguration.wizardWidth() );
       setPrefHeight( GraphWizardConfiguration.wizardHeight() );
    }// End Constructor
+   
+   /**
+    * Method to refresh the items in the {@link Graph} {@link ComboBox}.
+    */
+   private void refreshGraphs(){
+      graphBox.getItems().clear();
+      List< Graph > graphs = RequestSystem.retrieveAll( Graph.class );
+      graphs.forEach( graph -> graphBox.getItems().add( graph.getIdentification() ) );
+   }//End Method
 
    /**
     * {@inheritDoc}
@@ -65,8 +77,9 @@ public class GraphIntroductionPage extends VBox implements WizardPage< Graph > {
     * {@inheritDoc}
     */
    @Override public Node getContent( Graph graph ) {
+      refreshGraphs();
       if ( graph != null ) {
-         nameField.setText( graph.getIdentification() );
+         graphBox.getSelectionModel().select( graph.getIdentification() );
       }
       return this;
    }// End Method
@@ -75,7 +88,7 @@ public class GraphIntroductionPage extends VBox implements WizardPage< Graph > {
     * {@inheritDoc}
     */
    @Override public boolean isContentAcceptable() {
-      String name = nameField.getText();
+      String name = graphBox.getSelectionModel().getSelectedItem();
       if ( name == null || name.isEmpty() ) {
          Wizard.error( ERROR_HEADER, "No name has been specified." );
          return false;
@@ -98,7 +111,7 @@ public class GraphIntroductionPage extends VBox implements WizardPage< Graph > {
     * {@inheritDoc}
     */
    @Override public Graph configure( Graph configurable ) {
-      String name = nameField.getText();
+      String name = graphBox.getSelectionModel().getSelectedItem();
       Graph graph = RequestSystem.retrieve( Graph.class, name );
       if ( graph == null ) {
          graph = new Graph( name );
