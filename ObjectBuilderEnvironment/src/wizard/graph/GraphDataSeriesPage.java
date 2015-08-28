440cab60d55fcdfcfe2d8dac478937e8d4c010a2
DanGrew <danielanthonygrew@gmail.com>
2015-08-28 08:21:16 +0100

    GRAPH SEARCH WIZARD LAUNCH:
    Added button for launching the search wizard from the graph data page.
    Added refresh mechanism to graph page.

--- ObjectBuilderEnvironment/src/wizard/graph/GraphDataSeriesPage.java
+++ ObjectBuilderEnvironment/src/wizard/graph/GraphDataSeriesPage.java
@@ -7,20 +7,26 @@
  */
 package wizard.graph;
 
+import java.util.Arrays;
+import java.util.List;
+
+import architecture.data.DataManagementSystem;
+import architecture.data.SingletonStoredSource;
+import architecture.request.RequestSystem;
 import graphics.JavaFx;
+import graphics.event.JavaFxEventSystem;
 import graphics.node.DualListView;
 import graphics.wizard.Wizard;
 import graphics.wizard.WizardPage;
 import graphs.graph.Graph;
-
-import java.util.Arrays;
-import java.util.List;
-
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import menu.ObeGuiFunctions;
 import search.Search;
+import search.SearchSpace;
 import wizard.common.WizardConfiguration;
-import architecture.request.RequestSystem;
+import wizard.search.SearchSpaceWizard;
 
 /**
  * The {@link GraphDataSeriesPage} is responsible for showing the user how to configure
@@ -42,24 +48,52 @@ public class GraphDataSeriesPage extends VBox implements WizardPage< Graph >{
          ),
          JavaFx.wrappedLabel( 
                "Below you can choose any number of searches to add to the graph, each as their own "
-             + "series. Any search can be used and all of its matching objects will be plotted in"
-             + "the graph so long as they have the properties set in the previous steps. Please "
-             + "choose the data below."
-          )
+             + "series. Any search can be used and all of its matching objects will be plotted in "
+             + "the graph so long as they have the properties set in the previous steps. "
+         ),
+         JavaFx.wrappedLabel( 
+               "If there are no searches created, or you'd like a different set of data, you can launch "
+             + "the search wizard here."
+         )
+      ) );
+      
+      Button searchWizardButton = new Button();
+      getChildren().add( searchWizardButton );
+      ObeGuiFunctions.getSearchSpaceWizardFunction().configure( searchWizardButton );
+      searchWizardButton.setText( "Launch Search Wizard" );
+      
+      getChildren().addAll( Arrays.asList( 
+          JavaFx.wrappedLabel( 
+              "Please choose the search spaces to include in the graph."
+         )
       ) );
       
       searches = new DualListView<>( false );
       searches.setListWidth( WizardConfiguration.dualListWidth() );
       searches.setPrefHeight( WizardConfiguration.tableHeight() );
+      refresh();
+      JavaFxEventSystem.registerForEvent( DataManagementSystem.Events.ObjectStored, ( event, source ) -> { 
+         SingletonStoredSource wrapper = ( SingletonStoredSource )source;
+         if ( wrapper.classes.contains( SearchSpace.class ) ) {
+            refresh();
+         }
+      } );
+      getChildren().add( searches );
+      
+      setPrefWidth( WizardConfiguration.wizardWidth() );
+      setPrefHeight( WizardConfiguration.wizardHeight() + 150 );
+   }// End Constructor
+   
+   /**
+    * Method to refresh the table of {@link Search}s available. This captures any changes
+    * made by a launched {@link SearchSpaceWizard}.
+    */
+   private void refresh(){
       List< Search > searchObjects = RequestSystem.retrieveAll( 
                Search.class 
       );
       searches.setChoices( searchObjects );
-      getChildren().add( searches );
-      
-      setPrefWidth( WizardConfiguration.wizardWidth() );
-      setPrefHeight( WizardConfiguration.wizardHeight() + 100 );
-   }// End Constructor
+   }//End Method
    
    /**
     * {@inheritDoc}
@@ -72,6 +106,7 @@ public class GraphDataSeriesPage extends VBox implements WizardPage< Graph >{
     * {@inheritDoc}
     */
    @Override public Node getContent( Graph input ) {
+      refresh();
       List< Search > dataSeries = input.getDataSeries();
       if ( !dataSeries.isEmpty() ) {
          searches.setChosenItems( dataSeries );
