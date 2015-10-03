4c07cdc4b6758bc3266778eaadd63c26824ca8b0
DanGrew <danielanthonygrew@gmail.com>
2015-10-03 19:44:09 +0100

    CONTENT CONTROLLER TEST:
    Implemented test.

---
+++ ObjectBuilderEnvironmentTestHarness/src/diagram/layer/ContentControllerTest.java
@@ -0,0 +1,58 @@
+/*
+ * ----------------------------------------
+ *       Object Builder Environment
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
+package diagram.layer;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import architecture.event.EventSystem;
+import diagram.shapes.AddShapeEvent;
+import diagram.shapes.EllipticPolygon;
+import diagram.toolbox.ContentEvents;
+import javafx.embed.swing.JFXPanel;
+
+/**
+ * Test for the {@link ContentController}.
+ */
+public class ContentControllerTest {
+
+   /**
+    * Initialise method to launch JavaFx.
+    */
+   @BeforeClass public static void initialiseJavaFx(){
+      new JFXPanel();
+   }//End Method
+   
+   /**
+    * Test for adding a shape from event.
+    */
+   @Test public void shouldTriggerAddShape() {
+      Content content = Mockito.mock( Content.class );
+      new ContentController( content );
+      
+      EventSystem.raiseEvent( ContentEvents.AddShape, new AddShapeEvent( 100, -50 ) );
+      Mockito.verify( content ).addShape( 100, -50 );
+      Mockito.verifyNoMoreInteractions( content );
+   }//End Method
+   
+   /**
+    * Test for selecting a shape from event.
+    */
+   @Test public void shouldTriggerSelect() throws InterruptedException {
+      Content content = Mockito.mock( Content.class );
+      new ContentController( content );
+      
+      EllipticPolygon polygon = Mockito.mock( EllipticPolygon.class );
+      EventSystem.raiseEvent( ContentEvents.SelectNode, polygon );
+      Thread.sleep( 1000 );
+      Mockito.verify( content ).selectNode( polygon );
+      Mockito.verifyNoMoreInteractions( content );
+   }//End Method
+
+}//End Class
