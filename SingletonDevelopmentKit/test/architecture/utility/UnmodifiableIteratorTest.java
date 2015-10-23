36a82345be3bc08cbf307c0da8f806015e0cdc65
DanGrew <danielanthonygrew@gmail.com>
2015-10-23 22:11:00 +0100

    UNMODIFIABLE ITERATOR TEST:
    Implemented missing cases.

--- SingletonDevelopmentKit/test/architecture/utility/UnmodifiableIteratorTest.java
+++ SingletonDevelopmentKit/test/architecture/utility/UnmodifiableIteratorTest.java
@@ -1,13 +1,62 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package architecture.utility;
 
-import static org.junit.Assert.*;
+import java.util.Iterator;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+/**
+ * {@link UnmodifiableIterator} test.
+ */
 public class UnmodifiableIteratorTest {
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /**
+    * {@link UnmodifiableIterator#next()} and {@link UnmodifiableIterator#hasNext()} test.
+    */
+   @Test public void shouldForwardCalls() {
+      Iterator< ? > iterator = Mockito.mock( Iterator.class );
+      UnmodifiableIterator< ? > sut = new UnmodifiableIterator<>( iterator );
+      Mockito.verifyZeroInteractions( iterator );
+      
+      Mockito.verify( iterator, Mockito.times( 0 ) ).next();
+      Mockito.verify( iterator, Mockito.times( 0 ) ).hasNext();
+      
+      sut.next();
+      Mockito.verify( iterator, Mockito.times( 1 ) ).next();
+      sut.hasNext();
+      Mockito.verify( iterator, Mockito.times( 1 ) ).hasNext();
+      
+      sut.next();
+      sut.next();
+      sut.next();
+      Mockito.verify( iterator, Mockito.times( 4 ) ).next();
+      sut.hasNext();
+      sut.hasNext();
+      sut.hasNext();
+      Mockito.verify( iterator, Mockito.times( 4 ) ).hasNext();
+   }//End Method
+   
+   /**
+    * {@link UnmodifiableIterator#remove()} exception throwing test.
+    */
+   @Test( expected = UnsupportedOperationException.class )
+   public void shouldNotRemove() {
+      Iterator< ? > iterator = Mockito.mock( Iterator.class );
+      UnmodifiableIterator< ? > sut = new UnmodifiableIterator<>( iterator );
+      Mockito.verifyZeroInteractions( iterator );
+      
+      Mockito.verify( iterator, Mockito.times( 0 ) ).remove();
+      
+      sut.remove();
+      Assert.fail( "Should have thrown exception passing the test." );
+   }//End Method
 
-}
+}//End Class
