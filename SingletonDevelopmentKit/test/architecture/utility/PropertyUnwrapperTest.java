7d2b747db4d9d61a4480c5ec714f76febe3abf1e
DanGrew <danielanthonygrew@gmail.com>
2015-10-23 21:55:39 +0100

    PROPERTY UNWRAPPER TEST:
    Implemented missing cases.

--- SingletonDevelopmentKit/test/architecture/utility/PropertyUnwrapperTest.java
+++ SingletonDevelopmentKit/test/architecture/utility/PropertyUnwrapperTest.java
@@ -1,13 +1,87 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package architecture.utility;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * {@link PropertyUnwrapper} test.
+ */
 public class PropertyUnwrapperTest {
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /**
+    * {@link PropertyUnwrapper#next()} and {@link PropertyUnwrapper#hasNext()} test.
+    */
+   @Test public void shouldBehaveCorrectly() {
+      List< ReadOnlyStringProperty > properties = Arrays.asList( 
+               new SimpleStringProperty( "some" ),
+               new SimpleStringProperty( "text" ),
+               new SimpleStringProperty( "in" ),
+               new SimpleStringProperty( "properties" )
+      );
+      PropertyUnwrapper< String > sut = new PropertyUnwrapper<>( properties.iterator() );
+      String next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "some", next );
+      
+      //Prove does not read next
+      Assert.assertTrue( sut.hasNext() );
+      Assert.assertTrue( sut.hasNext() );
+      Assert.assertTrue( sut.hasNext() );
+      Assert.assertTrue( sut.hasNext() );
+      Assert.assertTrue( sut.hasNext() );
+      
+      next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "text", next );
+      next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "in", next );
+      next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "properties", next );
+   }//End Method
+   
+   /**
+    * {@link PropertyUnwrapper#remove()} test.
+    */
+   @Test public void shouldRemove() {
+      List< ReadOnlyStringProperty > properties = new ArrayList<>( Arrays.asList( 
+               new SimpleStringProperty( "some" ),
+               new SimpleStringProperty( "text" ),
+               new SimpleStringProperty( "in" ),
+               new SimpleStringProperty( "properties" )
+      ) );
+      
+      PropertyUnwrapper< String > sut = new PropertyUnwrapper<>( properties.iterator() );
+      String next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "some", next );
+      
+      //Comply with the remove interface
+      sut.next();
+      sut.remove();
+      Assert.assertEquals( 3, properties.size() );
+      sut.next();
+      sut.remove();
+      Assert.assertEquals( 2, properties.size() );
+      
+      next = sut.next();
+      Assert.assertNotNull( next );
+      Assert.assertEquals( "properties", next );
+   }//End Method
 
-}
+}//End Class
