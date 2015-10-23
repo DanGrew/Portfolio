226e5008d7616015cf44d19206a8ec0fa450ea1a
DanGrew <danielanthonygrew@gmail.com>
2015-10-23 21:41:19 +0100

    OBJECT GENERATOR TEST:
    Implemented missing test cases.

--- SingletonDevelopmentKit/test/architecture/utility/ObjectGeneratorTest.java
+++ SingletonDevelopmentKit/test/architecture/utility/ObjectGeneratorTest.java
@@ -1,13 +1,44 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package architecture.utility;
 
-import static org.junit.Assert.*;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import model.singleton.Singleton;
+
+/**
+ * {@link ObjectGenerator} test.
+ */
 public class ObjectGeneratorTest {
+   
+   /** Specific object for testing with basic constructor.**/
+   public static class TestObject {
+      /** Constructs a new {@link TestObject}. **/
+      public TestObject() {}
+   }//End Class
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /**
+    * {@link ObjectGenerator#construct(Class)} acceptance test.
+    */
+   @Test public void shouldConstruct() {
+      Assert.assertNotNull( ObjectGenerator.construct( String.class ) );
+      Assert.assertNotNull( ObjectGenerator.construct( TestObject.class ) );
+   }//End Method
+   
+   /**
+    * {@link ObjectGenerator#construct(Class)} reject test.
+    */
+   @Test public void shouldNotConstruct() {
+      Assert.assertNull( ObjectGenerator.construct( Map.class ) );
+      Assert.assertNull( ObjectGenerator.construct( Singleton.class ) );
+   }//End Method
 
-}
+}//End Class
