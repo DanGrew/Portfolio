dfa7ae01574b05bc31982a34e459def820e15fb2
DanGrew <danielanthonygrew@gmail.com>
2015-10-23 15:53:55 +0100

    IDENTIFICATION MATCHER TEST:

--- SingletonDevelopmentKit/test/architecture/utility/IdentificationMatcherTest.java
+++ SingletonDevelopmentKit/test/architecture/utility/IdentificationMatcherTest.java
@@ -1,13 +1,36 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package architecture.utility;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
+import test.model.TestObjects.TestSingleton;
+import test.model.TestObjects.TestSingletonImpl;
+
+/**
+ * {@link IdentificationMatcher} test.
+ */
 public class IdentificationMatcherTest {
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /**
+    * {@link IdentificationMatcher#test(model.singleton.Singleton)} test.
+    */
+   @Test public void shouldMatch() {
+      IdentificationMatcher< TestSingleton > sut = new IdentificationMatcher<>( "value" );
+      TestSingleton test1 = new TestSingletonImpl( "someValue" );
+      Assert.assertFalse( sut.test( test1 ) );
+      TestSingleton test2 = new TestSingletonImpl( "somevalue" );
+      Assert.assertFalse( sut.test( test2 ) );
+      TestSingleton test3 = new TestSingletonImpl( "something else" );
+      Assert.assertFalse( sut.test( test3 ) );
+      TestSingleton test4 = new TestSingletonImpl( "value" );
+      Assert.assertTrue( sut.test( test4 ) );
+   }//End Method
 
-}
+}//End Class
