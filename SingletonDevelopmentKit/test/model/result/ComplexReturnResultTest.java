cf7826718edddc53016b86bd8ca55d571f22c732
DanGrew <danielanthonygrew@gmail.com>
2015-10-27 13:33:28 +0000

    COMPLEX RETURN RESULT TEST:

--- SingletonDevelopmentKit/test/model/result/ComplexReturnResultTest.java
+++ SingletonDevelopmentKit/test/model/result/ComplexReturnResultTest.java
@@ -1,13 +1,34 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package model.result;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * {@link ComplexReturnResult} test.
+ */
 public class ComplexReturnResultTest {
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /** Test enum for results.**/
+   private enum TestEnum {
+      Failed, Passed
+   }//End Enum
+   
+   /**
+    * {@link ComplexReturnResult#getResult()} and {@link ComplexReturnResult#setResult(Enum)} test.
+    */
+   @Test public void shouldSetAndGet() {
+      ComplexReturnResult< TestEnum > result = new ComplexReturnResult<>();
+      result.setResult( TestEnum.Passed );
+      Assert.assertEquals( TestEnum.Passed, result.getResult() );
+      result.setResult( TestEnum.Failed );
+      Assert.assertEquals( TestEnum.Failed, result.getResult() );
+   }//End Method
 
-}
+}//End Class
