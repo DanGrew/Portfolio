dd0cee9ad267d13984a5ccf274373ca8affe2cf2
DanGrew <danielanthonygrew@gmail.com>
2015-10-28 10:08:33 +0000

    TEST COMMON TEST:
    Implemented missing cases.

--- SingletonDevelopmentKit/test/utility/TestCommonTest.java
+++ SingletonDevelopmentKit/test/utility/TestCommonTest.java
@@ -1,13 +1,85 @@
+/*
+ * ----------------------------------------
+ *        Singleton Development Kit
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ * ----------------------------------------
+ */
 package utility;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
+/**
+ * {@link TestCommon} test.
+ */
 public class TestCommonTest {
 
-   @Test public void test() {
-      fail( "Not yet implemented" );
-   }
+   /**
+    * {@link TestCommon#assertCollectionsSameOrderIrrelevant(java.util.Collection, java.util.Collection)} acceptance test.
+    */
+   @Test public void shouldAssertCollectionsSameOrderIndependent() {
+      List< String > a = Arrays.asList( "anything", "something", "nothing" );
+      List< String > b = Arrays.asList( "anything", "something", "nothing" );
+      TestCommon.assertCollectionsSameOrderIrrelevant( a, b );
+      
+      a = Arrays.asList( "anything", "anything", "nothing" );
+      b = Arrays.asList( "anything", "nothing", "anything" );
+      TestCommon.assertCollectionsSameOrderIrrelevant( a, b );
+   }//End Method
+   
+   /**
+    * {@link TestCommon#assertCollectionsSameOrderIrrelevant(java.util.Collection, java.util.Collection)} reject test.
+    */
+   @Test( expected = AssertionError.class ) 
+   public void shouldFailAssertCollectionsSameOrderIndependent() {
+      List< String > a = Arrays.asList( "anything", "something", "somethingElse" );
+      List< String > b = Arrays.asList( "anything", "something", "nothing" );
+      TestCommon.assertCollectionsSameOrderIrrelevant( a, b );
+   }//End Method
+   
+   /**
+    * {@link TestCommon#assertIterators(java.util.Iterator, java.util.Iterator)} acceptance test.
+    */
+   @Test public void shouldAssertIteratorsEquality(){
+      List< String > a = Arrays.asList( "anything", "something", "nothing" );
+      List< String > b = Arrays.asList( "anything", "something", "nothing" );
+      TestCommon.assertIterators( a.iterator(), b.iterator() );
+      
+      a = Arrays.asList( "anything", "anything", "nothing" );
+      b = Arrays.asList( "anything", "anything", "nothing" );
+      TestCommon.assertIterators( a.iterator(), b.iterator() );
+   }//End Method
+   
+   /**
+    * {@link TestCommon#assertIterators(java.util.Iterator, java.util.Iterator)} reject test.
+    */
+   @Test( expected = AssertionError.class ) 
+   public void shouldFailAssertIteratorsEquality(){
+      List< String > a = Arrays.asList( "anything", "something", "anything" );
+      List< String > b = Arrays.asList( "anything", "something", "nothing" );
+      TestCommon.assertIterators( a.iterator(), b.iterator() );
+   }//End Method
+   
+   /**
+    * {@link TestCommon#assertIterators(java.util.Iterator, java.util.Iterator)} reject for size mismatch.
+    */
+   @Test( expected = AssertionError.class ) 
+   public void shouldFailAssertIteratorsSize(){
+      List< String > a = Arrays.asList( "anything", "something", "anything", "anything" );
+      List< String > b = Arrays.asList( "anything", "something", "anything" );
+      TestCommon.assertIterators( a.iterator(), b.iterator() );
+   }//End Method
+   
+   /**
+    * {@link TestCommon#assertIterators(java.util.Iterator, java.util.Iterator, java.util.function.BiPredicate)} test.
+    */
+   @Test public void shouldAssertIteratorsCustomEquality(){
+      List< String > a = Arrays.asList( "a", "b", "c" );
+      List< String > b = Arrays.asList( "x", "y", "z" );
+      TestCommon.assertIterators( a.iterator(), b.iterator(), ( s, t ) -> { return s.compareTo( t ) < 0; } );
+   }//End Method
 
-}
+}//End Class
